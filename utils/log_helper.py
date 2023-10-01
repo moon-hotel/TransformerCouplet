97850a27ff0fd9aa446cd04a378cd83f601584bd
nulls8 <moon-hotel@hotmail.com>
2023-10-01 15:52:45 +0800

    优化日志模块

--- utils/log_helper.py
+++ utils/log_helper.py
@@ -1,36 +1,33 @@
+"""
+创建时间: 2023/3/3 9:09 下午
+作 者: @空字符
+公众号: @月来客栈
+知 乎: @月来客栈 https://www.zhihu.com/people/the_lastest
+"""
+
+from datetime import datetime
 import logging
 import os
-from datetime import datetime
+import sys
 
-class Logger(object):
 
-    def __init__(self, log_file_name, log_level, logger_name = "debug",log_dir = './logs/'):
-        # 创建一个logger
-        self.__logger = logging.getLogger(logger_name)
+def logger_init(log_file_name='monitor',
+                log_level=logging.DEBUG,
+                log_dir='./logs/',
+                only_file=False):
+    # 指定路径
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-        # 指定日志的最低输出级别，默认为WARN级别
-        self.__logger.setLevel(log_level)
+    log_path = os.path.join(log_dir, log_file_name + '_' + str(datetime.now())[:10] + '.txt')
+    formatter = '[%(asctime)s] - %(levelname)s: [%(filename)s][%(lineno)s] %(message)s'
+    datefmt = "%Y-%m-%d %H:%M:%S'"
+    if only_file:
+        logging.basicConfig(filename=log_path, level=log_level, format=formatter, datefmt=datefmt)
+    else:
+        logging.basicConfig(level=log_level, format=formatter, datefmt=datefmt,
+                            handlers=[logging.FileHandler(log_path),
+                                      logging.StreamHandler(sys.stdout)])
 
-        # 指定路径
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-        # 创建一个handler用于写入日志文件
-        log_path = os.path.join(log_dir,log_file_name+'_'+str(datetime.now())[:10]+'.txt')
-        file_handler = logging.FileHandler(log_path)
 
-        # 创建一个handler用于输出控制台
-        console_handler = logging.StreamHandler()
-
-        # 定义handler的输出格式
-        console_formatter = logging.Formatter('-- %(levelname)s: %(message)s')
-        file_formatter = logging.Formatter('[%(asctime)s] - %(levelname)s: %(message)s')
-
-        file_handler.setFormatter(file_formatter)
-        console_handler.setFormatter(console_formatter)
-
-        # 给logger添加handler
-        self.__logger.addHandler(file_handler)
-        self.__logger.addHandler(console_handler)
-
-    def get_log(self):
-        return self.__logger
+logger_init(log_file_name='log', log_level=logging.DEBUG, log_dir='./log')
