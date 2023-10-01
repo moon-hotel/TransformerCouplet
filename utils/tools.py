ca943490e8d5c69a1b81840fbbb0687c9ee6ad78
nulls8 <moon-hotel@hotmail.com>
2023-10-01 15:55:54 +0800

    加入预处理缓存修饰器

---
+++ utils/tools.py
@@ -0,0 +1,78 @@
+"""
+文件名: Code/utils/tools.py
+创建时间: 2023/4/12 8:27 下午
+作 者: @空字符
+公众号: @月来客栈
+知 乎: @月来客栈 https://www.zhihu.com/people/the_lastest
+"""
+
+import torch
+import logging
+import time
+import os
+import re
+
+
+def contains_chinese(text):
+    pattern = re.compile(r'[\u4e00-\u9fff]')  # 匹配中文字符的正则表达式
+    return bool(re.search(pattern, text))
+
+
+def get_gpus(num=None):
+    """
+    如果num为list，则返回list中对应的GPU；
+    如果num为整数，则返回前num个gpu
+    如果没有GPU则返回CPU
+    :param num:
+    :return:
+    """
+    gpu_nums = torch.cuda.device_count()
+    if isinstance(num, list):
+        devices = [torch.device(f'cuda:{i}')
+                   for i in num if i < gpu_nums]
+    else:
+        devices = [torch.device(f'cuda:{i}')
+                   for i in range(gpu_nums)][:num]
+    return devices if devices else [torch.device('cpu')]
+
+
+def process_cache(unique_key=None):
+    """
+    数据预处理结果缓存修饰器
+    :param : unique_key
+    :return:
+    """
+    if unique_key is None:
+        raise ValueError(
+            "unique_key 不能为空, 请指定相关数据集构造类的成员变量，如['top_k', 'cut_words', 'max_sen_len']")
+
+    def decorating_function(func):
+        def wrapper(*args, **kwargs):
+            logging.info(f" ## 索引预处理缓存文件的参数为：{unique_key}")
+            obj = args[0]  # 获取类对象，因为data_process(self, file_path=None)中的第1个参数为self
+            file_path = kwargs['file_path']
+            file_dir = f"{os.sep}".join(file_path.split(os.sep)[:-1])
+            file_name = "".join(file_path.split(os.sep)[-1].split('.')[:-1])
+            paras = f"cache_{file_name}_"
+            for k in unique_key:
+                paras += f"{k}{obj.__dict__[k]}_"  # 遍历对象中的所有参数
+            cache_path = os.path.join(file_dir, paras[:-1] + '.pt')
+            start_time = time.time()
+            if not os.path.exists(cache_path):
+                logging.info(f"缓存文件 {cache_path} 不存在，重新处理并缓存！")
+                data = func(*args, **kwargs)
+                with open(cache_path, 'wb') as f:
+                    torch.save(data, f)
+            else:
+                logging.info(f"缓存文件 {cache_path} 存在，直接载入缓存文件！")
+                with open(cache_path, 'rb') as f:
+                    data = torch.load(f)
+            end_time = time.time()
+            logging.info(f"数据预处理一共耗时{(end_time - start_time):.3f}s")
+            return data
+
+        return wrapper
+
+    return decorating_function
+
+
